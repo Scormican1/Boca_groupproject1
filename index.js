87ee8ef52d7862bab8a7f107a49c389e036358c4
Sara Cormican <S_Comican@yahoo.com>
2022-01-20 14:33:40 -0500

    upload project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // new project
+function readThisFile(file) {
+    const fs = require('fs');
+    const data = fs.readFileSync('data.json', 'utf-8');
+    return data;
+}
+    console.log(readThisFile());
+
+    const fs = require('fs');
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log (err);
+        }
+        console.log (data)
+    })
+
+
+
